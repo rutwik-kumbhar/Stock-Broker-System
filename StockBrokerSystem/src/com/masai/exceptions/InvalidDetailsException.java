ca018b225f12cf1ee92cf275c27db71579fc5428
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2023-03-31 00:00:06 +0530

    Addmin Login funtinality added

---
+++ StockBrokerSystem/src/com/masai/exceptions/InvalidDetailsException.java
@@ -0,0 +1,14 @@
+package com.masai.exceptions;
+
+public class InvalidDetailsException extends Exception {
+	
+	
+
+	public InvalidDetailsException(String msg) {
+		super(msg);
+		
+	}
+	
+	
+
+}
---
+++ StockBrokerSystem/src/com/masai/exceptions/StockException.java
@@ -0,0 +1,10 @@
+package com.masai.exceptions;
+
+public class StockException extends Exception{
+
+	public StockException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
